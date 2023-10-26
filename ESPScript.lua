0bb0710efd63b54938e435e4101947ffedba1e48
Aerostyx <112239166+Aerostyx@users.noreply.github.com>
2023-10-26 13:39:21 -0400

    Create ESPScript.lua

---
+++ ESPScript.lua
@@ -0,0 +1,12 @@
+-- I made this in school with no syntax correction or anything so it probably doesn't even work.
+
+local player = game.Players.LocalPlayer
+
+local GuiInstance = Instance.new("ScreenGui")
+local MainFrame = Instance.new("Frame")
+local Label = Instance.new("TextLabel")
+
+GuiInstance.Parent = player:WaitForChild("PlayerGui")
+
+MainFrame.Parent = GuiInstance
+MainFrame.Size = UDim2.new('0, 300, 0, 300')
